fa013c93d7cbe084672a32ab4af5437bac7c4ca6
Lexxick <sydazm06@gmail.com>
2026-01-06 23:17:04 +0800

    fix: update user_data.sh to use ssm-user instead of ubuntu for ansible directory and file permissions

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -5,17 +5,17 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 apt-get update -y
 apt-get install -y git python3-pip ansible
 
-mkdir -p /home/ubuntu/.ansible
+mkdir -p /home/ssm-user/.ansible
 
-cat > /home/ubuntu/.ansible/inventory.ini <<'INVENTORY_EOF'
+cat > /home/ssm-user/.ansible/inventory.ini <<'INVENTORY_EOF'
 ${inventory_content}
 INVENTORY_EOF
 
-cat > /home/ubuntu/.ansible/ansible_key.pem <<'KEY_EOF'
+cat > /home/ssm-user/.ansible/ansible_key.pem <<'KEY_EOF'
 ${key_content}
 KEY_EOF
 
-chmod 600 /home/ubuntu/.ansible/ansible_key.pem
-chown -R ubuntu:ubuntu /home/ubuntu/.ansible
+chmod 600 /home/ssm-user/.ansible/ansible_key.pem
+chown -R ssm-user:ssm-user /home/ssm-user/.ansible
 
 echo "ansible files written"
